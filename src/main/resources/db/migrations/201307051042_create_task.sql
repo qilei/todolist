eb32abc8d4fc8a8520ea01cc0be6f3e9d232b9c5
qilei <qilei2010@gmail.com>
2013-07-13 10:28:10 +0800

    表结构变更

--- src/main/resources/db/migrations/201307051042_create_task.sql
+++ src/main/resources/db/migrations/201307051042_create_task.sql
@@ -1,8 +1,9 @@
 CREATE TABLE IF NOT EXISTS `task` (
   `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `due_date` datetime DEFAULT NULL,
-  `duration` int(11) DEFAULT NULL,
+  `duration` varchar(50) DEFAULT NULL,
   `item` varchar(255) DEFAULT NULL,
-  `priority` varchar(255) DEFAULT NULL,
+  `priority` varchar(50) DEFAULT NULL,
+  `is_finish` tinyint DEFAULT '0' NULL,
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
